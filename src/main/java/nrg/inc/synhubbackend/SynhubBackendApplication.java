f008135111293cf45fd3150c8d8e08bc9577c251
Al3xiel <gabriel280104casas@gmail.com>
2025-04-24 01:34:11 -0500

    chore: setup project.

---
+++ src/main/java/nrg/inc/synhubbackend/SynhubBackendApplication.java
@@ -0,0 +1,13 @@
+package nrg.inc.synhubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynhubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynhubBackendApplication.class, args);
+    }
+
+}
