c33d18f1c68ce787911430e5aa0f059605ca8161
cwassointt <dillpixel1@gmail.com>
2025-05-11 23:58:37 -0500

    feat(application): add GenerateReportCommandHandler to handle report generation commands

---
+++ src/main/java/nrg/inc/synhubbackend/analytics/application/command/GenerateReportCommandHandler.java
@@ -0,0 +1,17 @@
+package nrg.inc.synhubbackend.analytics.application.command;
+
+import nrg.inc.synhubbackend.analytics.domain.model.Report;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GenerateReportCommandHandler {
+    private final ReportCommandService reportCommandService;
+
+    public GenerateReportCommandHandler(ReportCommandService reportCommandService) {
+        this.reportCommandService = reportCommandService;
+    }
+
+    public Report handle(Long userId) {
+        return reportCommandService.generateReport(userId);
+    }
+}
