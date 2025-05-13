395099d3d5826d3587899ff0671d93d1f268a2bc
NeoRise <defneorise@gmail.com>
2025-05-13 16:43:54 -0500

    feat(groups): add commands

---
+++ src/main/java/nrg/inc/synhubbackend/groups/domain/model/commands/AddMemberCommand.java
@@ -0,0 +1,5 @@
+package nrg.inc.synhubbackend.groups.domain.model.commands;
+
+public record AddMemberCommand(Long memberId, Long groupId) {
+}
+
---
+++ src/main/java/nrg/inc/synhubbackend/groups/domain/model/commands/CreateGroupCommand.java
@@ -0,0 +1,7 @@
+package nrg.inc.synhubbackend.groups.domain.model.commands;
+
+public record CreateGroupCommand(
+        String name,
+        String imgUrl
+) {
+}
