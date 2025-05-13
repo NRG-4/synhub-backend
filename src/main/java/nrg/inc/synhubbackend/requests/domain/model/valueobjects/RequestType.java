69f3b14288ce547c64b6807e8b3531d81689f8f8
pachieeee <sebaspacheco2003@gmail.com>
2025-05-13 11:09:44 -0500

    feat(value-objects): add constants to request type and request status value objects.

--- src/main/java/nrg/inc/synhubbackend/requests/domain/model/valueobjects/RequestType.java
+++ src/main/java/nrg/inc/synhubbackend/requests/domain/model/valueobjects/RequestType.java
@@ -1,4 +1,15 @@
 package nrg.inc.synhubbackend.requests.domain.model.valueobjects;
 
 public enum RequestType {
+    TASK_SUBMISSION,
+    TASK_MODIFICATION;
+
+    public static RequestType fromString(String type) {
+        for (RequestType requestType : RequestType.values()) {
+            if (requestType.name().equalsIgnoreCase(type)) {
+                return requestType;
+            }
+        }
+        throw new IllegalArgumentException("Unknown type: " + type);
+    }
 }
