1de3aed374c3b103ee9c84b10add976a223efa74
pachieeee <sebaspacheco2003@gmail.com>
2025-05-13 14:42:28 -0500

    feat(entities): add lombok annotations and add update request status functionality.

--- src/main/java/nrg/inc/synhubbackend/requests/domain/model/aggregates/Request.java
+++ src/main/java/nrg/inc/synhubbackend/requests/domain/model/aggregates/Request.java
@@ -1,26 +1,45 @@
 package nrg.inc.synhubbackend.requests.domain.model.aggregates;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NonNull;
 import nrg.inc.synhubbackend.requests.domain.model.commands.CreateRequestCommand;
 import nrg.inc.synhubbackend.requests.domain.model.valueobjects.RequestStatus;
 import nrg.inc.synhubbackend.requests.domain.model.valueobjects.RequestType;
 import nrg.inc.synhubbackend.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
+@Getter
 @Entity
+@Table(name = "requests")
 public class Request extends AuditableAbstractAggregateRoot<Request> {
+    @NonNull
     private String description;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "request_type")
     private RequestType requestType;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "request_status")
     private RequestStatus requestStatus;
 
+    @NonNull
+    @Column(name = "task_id")
     private Long taskId;
 
+    @NonNull
+    @Column(name = "member_id")
     private Long memberId;
 
     public Request() {
-        this.requestStatus = RequestStatus.PENDING;
-        this.requestType = RequestType.SUBMISSION;
+    }
+
+    public String getRequestType() {
+        return requestType.toString();
+    }
+
+    public String getRequestStatus() {
+        return requestStatus.toString();
     }
 
     public Request(String description, RequestType requestType, RequestStatus requestStatus, Long taskId, Long memberId) {
@@ -37,5 +56,10 @@ public class Request extends AuditableAbstractAggregateRoot<Request> {
         this.requestStatus = RequestStatus.PENDING;
         this.taskId = command.taskId();
         this.memberId = command.memberId();
+    }
+
+    public Request updateRequestStatus(String requestStatus) {
+        this.requestStatus = RequestStatus.fromString(requestStatus);
+        return this;
     }
 }
