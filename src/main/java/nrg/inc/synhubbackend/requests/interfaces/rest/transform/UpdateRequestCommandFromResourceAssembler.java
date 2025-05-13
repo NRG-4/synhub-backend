37d414d381fc7e63c352d2974f2dd422e7b66850
pachieeee <sebaspacheco2003@gmail.com>
2025-05-13 15:41:38 -0500

    fix(resources): fix update method to update request from received status instead of the whole entity, optimizing usage.

--- src/main/java/nrg/inc/synhubbackend/requests/interfaces/rest/transform/UpdateRequestCommandFromResourceAssembler.java
+++ src/main/java/nrg/inc/synhubbackend/requests/interfaces/rest/transform/UpdateRequestCommandFromResourceAssembler.java
@@ -4,7 +4,7 @@ import nrg.inc.synhubbackend.requests.domain.model.commands.UpdateRequestCommand
 import nrg.inc.synhubbackend.requests.interfaces.rest.resources.RequestResource;
 
 public class UpdateRequestCommandFromResourceAssembler {
-    public static UpdateRequestCommand toCommandFromResource(Long requestId, RequestResource resource) {
-        return new UpdateRequestCommand(requestId, resource.requestStatus());
+    public static UpdateRequestCommand toCommandFromResource(Long requestId, String status) {
+        return new UpdateRequestCommand(requestId, status);
     }
 }
