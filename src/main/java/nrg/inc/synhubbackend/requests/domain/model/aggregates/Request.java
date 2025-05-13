b856b1e2d4031d5a763ef4e747ffcd30783caaee
pachieeee <sebaspacheco2003@gmail.com>
2025-05-13 11:03:35 -0500

    feat(aggregates): add values to request aggregate.

--- src/main/java/nrg/inc/synhubbackend/requests/domain/model/aggregates/Request.java
+++ src/main/java/nrg/inc/synhubbackend/requests/domain/model/aggregates/Request.java
@@ -1,4 +1,19 @@
 package nrg.inc.synhubbackend.requests.domain.model.aggregates;
 
-public class Request {
+import jakarta.persistence.Entity;
+import nrg.inc.synhubbackend.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+
+@Entity
+public class Request extends AuditableAbstractAggregateRoot<Request> {
+    private String description;
+
+    //TODO: Make this a reference to a RequestType entity
+    private String type;
+
+    //TODO: Make this a reference to a RequestStatus entity
+    private String status;
+
+    private int taskId;
+
+    private int memberId;
 }
