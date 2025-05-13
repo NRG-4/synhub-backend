55e7d0f033f611acd82c842666bbdad9a8ec0b83
NeoRise <defneorise@gmail.com>
2025-05-13 16:44:01 -0500

    feat(groups): add command service

---
+++ src/main/java/nrg/inc/synhubbackend/groups/domain/services/GroupCommandService.java
@@ -0,0 +1,13 @@
+package nrg.inc.synhubbackend.groups.domain.services;
+
+import nrg.inc.synhubbackend.groups.domain.model.aggregates.Group;
+import nrg.inc.synhubbackend.groups.domain.model.commands.CreateGroupCommand;
+import nrg.inc.synhubbackend.groups.domain.model.commands.DeleteGroupCommand;
+import nrg.inc.synhubbackend.groups.domain.model.commands.UpdateGroupCommand;
+import java.util.Optional;
+
+public interface GroupCommandService {
+    Optional<Group> handle(CreateGroupCommand command);
+    Optional<Group> handle(UpdateGroupCommand command);
+    Boolean handle(DeleteGroupCommand command);
+}
