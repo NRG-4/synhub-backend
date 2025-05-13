13032c1dd17b58b229e149303b510c17d58db7fd
pachieeee <sebaspacheco2003@gmail.com>
2025-05-13 11:50:10 -0500

    feat(services): add methods for request command and query services.

--- src/main/java/nrg/inc/synhubbackend/requests/domain/services/RequestCommandService.java
+++ src/main/java/nrg/inc/synhubbackend/requests/domain/services/RequestCommandService.java
@@ -1,4 +1,7 @@
 package nrg.inc.synhubbackend.requests.domain.services;
 
+import nrg.inc.synhubbackend.requests.domain.model.commands.CreateRequestCommand;
+
 public interface RequestCommandService {
+    Long handle(CreateRequestCommand command);
 }
--- src/main/java/nrg/inc/synhubbackend/requests/domain/services/RequestQueryService.java
+++ src/main/java/nrg/inc/synhubbackend/requests/domain/services/RequestQueryService.java
@@ -1,4 +1,15 @@
 package nrg.inc.synhubbackend.requests.domain.services;
 
+import nrg.inc.synhubbackend.requests.domain.model.aggregates.Request;
+import nrg.inc.synhubbackend.requests.domain.model.queries.GetAllRequestsQuery;
+import nrg.inc.synhubbackend.requests.domain.model.queries.GetRequestByIdQuery;
+import nrg.inc.synhubbackend.requests.domain.model.queries.GetRequestByTaskIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface RequestQueryService {
+    List<Request> handle(GetAllRequestsQuery query);
+    Optional<Request> handle(GetRequestByTaskIdQuery query);
+    Optional<Request> handle(GetRequestByIdQuery query);
 }
