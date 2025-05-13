c9d6f9072548ea8b257ad3c20fcbe746465dca3c
pachieeee <sebaspacheco2003@gmail.com>
2025-05-13 14:43:47 -0500

    feat(interfaces): add assembler for request creation command and request update command.

--- src/main/java/nrg/inc/synhubbackend/requests/interfaces/rest/transform/CreateRequestCommandFromResourceAssembler.java
+++ src/main/java/nrg/inc/synhubbackend/requests/interfaces/rest/transform/CreateRequestCommandFromResourceAssembler.java
@@ -1,4 +1,15 @@
 package nrg.inc.synhubbackend.requests.interfaces.rest.transform;
 
+import nrg.inc.synhubbackend.requests.domain.model.commands.CreateRequestCommand;
+import nrg.inc.synhubbackend.requests.interfaces.rest.resources.CreateRequestResource;
+
 public class CreateRequestCommandFromResourceAssembler {
+    public static CreateRequestCommand toCommandFromResource(CreateRequestResource resource) {
+        return new CreateRequestCommand(
+                resource.description(),
+                resource.requestType(),
+                resource.taskId(),
+                resource.memberId()
+        );
+    }
 }
