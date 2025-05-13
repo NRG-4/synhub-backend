a79a674e597ee48bb12bd8d434eba787266e5c35
pachieeee <sebaspacheco2003@gmail.com>
2025-05-13 12:11:31 -0500

    fix(services): add service implementation for request command and query.

--- src/main/java/nrg/inc/synhubbackend/requests/application/internal/commandservices/RequestCommandServiceImpl.java
+++ src/main/java/nrg/inc/synhubbackend/requests/application/internal/commandservices/RequestCommandServiceImpl.java
@@ -1,4 +1,28 @@
 package nrg.inc.synhubbackend.requests.application.internal.commandservices;
 
-public class RequestCommandServiceImpl {
+import nrg.inc.synhubbackend.requests.domain.model.aggregates.Request;
+import nrg.inc.synhubbackend.requests.domain.model.commands.CreateRequestCommand;
+import nrg.inc.synhubbackend.requests.domain.services.RequestCommandService;
+import nrg.inc.synhubbackend.requests.infrastructure.persistence.jpa.repositories.RequestRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RequestCommandServiceImpl implements RequestCommandService {
+
+    private final RequestRepository requestRepository;
+
+    public RequestCommandServiceImpl(RequestRepository requestRepository) {
+        this.requestRepository = requestRepository;
+    }
+
+    @Override
+    public Long handle(CreateRequestCommand command) {
+        if (this.requestRepository.existsByTaskId(command.taskId())) {
+            throw new IllegalArgumentException("A request already exists for current task");
+        }
+
+        var request = new Request(command);
+        this.requestRepository.save(request);
+        return request.getId();
+    }
 }
--- src/main/java/nrg/inc/synhubbackend/requests/application/internal/queryservices/RequestQueryServiceImpl.java
+++ src/main/java/nrg/inc/synhubbackend/requests/application/internal/queryservices/RequestQueryServiceImpl.java
@@ -1,4 +1,37 @@
 package nrg.inc.synhubbackend.requests.application.internal.queryservices;
 
-public class RequestQueryServiceImpl {
+import nrg.inc.synhubbackend.requests.domain.model.aggregates.Request;
+import nrg.inc.synhubbackend.requests.domain.model.queries.GetAllRequestsQuery;
+import nrg.inc.synhubbackend.requests.domain.model.queries.GetRequestByIdQuery;
+import nrg.inc.synhubbackend.requests.domain.model.queries.GetRequestByTaskIdQuery;
+import nrg.inc.synhubbackend.requests.domain.services.RequestQueryService;
+import nrg.inc.synhubbackend.requests.infrastructure.persistence.jpa.repositories.RequestRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RequestQueryServiceImpl implements RequestQueryService {
+
+    private final RequestRepository requestRepository;
+
+    public RequestQueryServiceImpl(RequestRepository requestRepository) {
+        this.requestRepository = requestRepository;
+    }
+
+    @Override
+    public List<Request> handle(GetAllRequestsQuery query) {
+        return requestRepository.findAll();
+    }
+
+    @Override
+    public Optional<Request> handle(GetRequestByTaskIdQuery query) {
+        return requestRepository.findByTaskId(query.taskId());
+    }
+
+    @Override
+    public Optional<Request> handle(GetRequestByIdQuery query) {
+        return this.requestRepository.findByRequestId(query.requestId());
+    }
 }
