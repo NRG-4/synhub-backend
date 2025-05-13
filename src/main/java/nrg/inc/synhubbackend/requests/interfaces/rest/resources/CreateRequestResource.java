5b1b275a63cef16439a447c6bd5871712cd3a519
pachieeee <sebaspacheco2003@gmail.com>
2025-05-13 12:10:55 -0500

    feat(resources): added values for create request resource.

--- src/main/java/nrg/inc/synhubbackend/requests/interfaces/rest/resources/CreateRequestResource.java
+++ src/main/java/nrg/inc/synhubbackend/requests/interfaces/rest/resources/CreateRequestResource.java
@@ -1,4 +1,9 @@
 package nrg.inc.synhubbackend.requests.interfaces.rest.resources;
 
-public record CreateRequestResource() {
+public record CreateRequestResource(
+        String description,
+        String requestType,
+        Long taskId,
+        Long  memberId
+) {
 }
