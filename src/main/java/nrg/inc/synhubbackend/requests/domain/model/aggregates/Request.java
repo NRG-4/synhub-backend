49677337d46935f62804e4e8a37354882d7e6e88
pachieeee <sebaspacheco2003@gmail.com>
2025-05-13 11:50:39 -0500

    fix(services): change request type and status to a proper enumeration value.

--- src/main/java/nrg/inc/synhubbackend/requests/domain/model/aggregates/Request.java
+++ src/main/java/nrg/inc/synhubbackend/requests/domain/model/aggregates/Request.java
@@ -1,19 +1,41 @@
 package nrg.inc.synhubbackend.requests.domain.model.aggregates;
 
 import jakarta.persistence.Entity;
+import nrg.inc.synhubbackend.requests.domain.model.commands.CreateRequestCommand;
+import nrg.inc.synhubbackend.requests.domain.model.valueobjects.RequestStatus;
+import nrg.inc.synhubbackend.requests.domain.model.valueobjects.RequestType;
 import nrg.inc.synhubbackend.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
 @Entity
 public class Request extends AuditableAbstractAggregateRoot<Request> {
     private String description;
 
-    //TODO: Make this a reference to a RequestType entity
-    private String type;
+    private RequestType requestType;
 
-    //TODO: Make this a reference to a RequestStatus entity
-    private String status;
+    private RequestStatus requestStatus;
 
-    private int taskId;
+    private Long taskId;
 
-    private int memberId;
+    private Long memberId;
+
+    public Request() {
+        this.requestStatus = RequestStatus.PENDING;
+        this.requestType = RequestType.TASK_SUBMISSION;
+    }
+
+    public Request(String description, RequestType requestType, RequestStatus requestStatus, Long taskId, Long memberId) {
+        this.description = description;
+        this.requestType = requestType;
+        this.requestStatus = requestStatus;
+        this.taskId = taskId;
+        this.memberId = memberId;
+    }
+
+    public Request(CreateRequestCommand command) {
+        this.description = command.description();
+        this.requestType = RequestType.fromString(command.type());
+        this.requestStatus = RequestStatus.PENDING;
+        this.taskId = command.taskId();
+        this.memberId = command.memberId();
+    }
 }
