0232571fce2540457103bc3298ac08507434336c
cwassointt <dillpixel1@gmail.com>
2025-05-14 01:03:59 -0500

    feat: Add MemberData DTO for member information

---
+++ src/main/java/nrg/inc/synhubbackend/analytics/domain/model/MemberData.java
@@ -0,0 +1,11 @@
+package nrg.inc.synhubbackend.analytics.domain.model;
+
+public class MemberData {
+    private final String name;
+
+    public MemberData(String name) {
+        this.name = name;
+    }
+
+    public String getName() { return name; }
+}
