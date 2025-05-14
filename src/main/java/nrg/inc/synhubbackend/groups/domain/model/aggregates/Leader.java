06d922b76ffc2fec67b95c63e91fe974df7b7dcf
Firtness <u202122484@upc.edu.pe>
2025-05-14 02:29:25 -0500

    feat(leader): add leader controller

--- src/main/java/nrg/inc/synhubbackend/groups/domain/model/aggregates/Leader.java
+++ src/main/java/nrg/inc/synhubbackend/groups/domain/model/aggregates/Leader.java
@@ -3,15 +3,21 @@ package nrg.inc.synhubbackend.groups.domain.model.aggregates;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import nrg.inc.synhubbackend.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Leader extends AuditableAbstractAggregateRoot<Leader> {
 
     @NotNull
     String name;
+
+    public Leader(String name) {
+        this.name = name;
+    }
 
 }
---
+++ src/main/java/nrg/inc/synhubbackend/groups/interfaces/rest/LeaderController.java
@@ -0,0 +1,50 @@
+package nrg.inc.synhubbackend.groups.interfaces.rest;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import nrg.inc.synhubbackend.groups.domain.model.commands.CreateLeaderCommand;
+import nrg.inc.synhubbackend.groups.domain.model.queries.GetLeaderByIdQuery;
+import nrg.inc.synhubbackend.groups.domain.services.LeaderCommandService;
+import nrg.inc.synhubbackend.groups.domain.services.LeaderQueryService;
+import nrg.inc.synhubbackend.groups.interfaces.rest.resources.LeaderResource;
+import nrg.inc.synhubbackend.groups.interfaces.rest.transform.LeaderResourceFromEntityAssembler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/v1/leaders")
+@Tag(name = "Leaders", description = "Leader management API")
+public class LeaderController {
+
+    private final LeaderQueryService leaderQueryService;
+    private final LeaderCommandService leaderCommandService;
+
+    public LeaderController(LeaderQueryService leaderQueryService, LeaderCommandService leaderCommandService) {
+        this.leaderQueryService = leaderQueryService;
+        this.leaderCommandService = leaderCommandService;
+    }
+
+    @GetMapping("/{leaderId}")
+    @Operation(summary = "Get a leader by ID", description = "Gets a leader by ID")
+    public ResponseEntity<LeaderResource> getLeaderById(@PathVariable Long leaderId) {
+        var getLeaderByIdQuery = new GetLeaderByIdQuery(leaderId);
+        var leader = this.leaderQueryService.handle(getLeaderByIdQuery);
+
+        if (leader.isEmpty()) return ResponseEntity.notFound().build();
+
+        var leaderResource = LeaderResourceFromEntityAssembler.toResourceFromEntity(leader.get());
+        return ResponseEntity.ok(leaderResource);
+    }
+
+    @PostMapping
+    @Operation(summary = "Create a new leader", description = "Creates a new leader")
+    public ResponseEntity<LeaderResource> createLeader(@RequestBody LeaderResource leaderResource) {
+        var createLeaderCommand = new CreateLeaderCommand(leaderResource.name());
+        var leader = this.leaderCommandService.handle(createLeaderCommand);
+
+        var createdLeaderResource = LeaderResourceFromEntityAssembler.toResourceFromEntity(leader.get());
+        return ResponseEntity.status(201).body(createdLeaderResource);
+    }
+
+
+}
