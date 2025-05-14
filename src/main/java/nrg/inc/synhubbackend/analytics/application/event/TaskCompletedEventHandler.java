55d32131101c2bdf4778fe082994e9e67e90e3a8
cwassointt <dillpixel1@gmail.com>
2025-05-14 01:05:20 -0500

    feat: Implement TaskCompletedEventHandler for task completion events

---
+++ src/main/java/nrg/inc/synhubbackend/analytics/application/event/TaskCompletedEventHandler.java
@@ -0,0 +1,44 @@
+package nrg.inc.synhubbackend.analytics.application.event;
+
+import nrg.inc.synhubbackend.analytics.domain.model.ActivityLog;
+import nrg.inc.synhubbackend.analytics.domain.repository.ActivityLogRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+public class TaskCompletedEventHandler {
+    private final ActivityLogRepository activityLogRepository;
+
+    public TaskCompletedEventHandler(ActivityLogRepository activityLogRepository) {
+        this.activityLogRepository = activityLogRepository;
+    }
+
+    @TransactionalEventListener
+    public void handleTaskCompletedEvent(TaskCompletedEvent event) {
+        ActivityLog log = new ActivityLog(
+                event.getUserId(),
+                "task_completed",
+                "taskId=" + event.getTaskId()
+        );
+        log.createLog();
+        activityLogRepository.save(log);
+    }
+}
+
+class TaskCompletedEvent {
+    private Long userId;
+    private Long taskId;
+
+    public TaskCompletedEvent(Long userId, Long taskId) {
+        this.userId = userId;
+        this.taskId = taskId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+}
