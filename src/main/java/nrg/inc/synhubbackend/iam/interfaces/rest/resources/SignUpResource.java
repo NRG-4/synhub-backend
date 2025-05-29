a006121d463c8a109eaae9d963292d53f4b95141
Al3xiel <gabriel280104casas@gmail.com>
2025-05-28 19:06:49 -0500

    feat(jwt): added resources.

---
+++ src/main/java/nrg/inc/synhubbackend/iam/interfaces/rest/resources/SignUpResource.java
@@ -0,0 +1,7 @@
+package nrg.inc.synhubbackend.iam.interfaces.rest.resources;
+
+import java.util.List;
+
+public record SignUpResource(String username, String password, List<String> roles) {
+}
+
---
+++ src/main/java/nrg/inc/synhubbackend/iam/interfaces/rest/resources/UserResource.java
@@ -0,0 +1,6 @@
+package nrg.inc.synhubbackend.iam.interfaces.rest.resources;
+
+import java.util.List;
+
+public record UserResource(Long id, String username, List<String> roles) {
+}
