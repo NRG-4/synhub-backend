cd1e03a1d5c0b8422b145054bd4e7398f90f08d7
Al3xiel <gabriel280104casas@gmail.com>
2025-05-28 21:57:00 -0500

    feat(iam): added assemblers.

--- src/main/java/nrg/inc/synhubbackend/iam/interfaces/rest/transform/UserResourceFromEntityAssembler.java
+++ src/main/java/nrg/inc/synhubbackend/iam/interfaces/rest/transform/UserResourceFromEntityAssembler.java
@@ -10,13 +10,39 @@ public class UserResourceFromEntityAssembler {
     var roles = user.getRoles().stream()
         .map(Role::getStringName)
         .toList();
-    return new UserResource(
-            user.getId(),
-            user.getUsername(),
-            user.getName(),
-            user.getSurname(),
-            user.getImgUrl(),
-            user.getEmail(),
-            roles);
+    if(user.getLeader() != null) {
+      return new UserResource(
+              user.getId(),
+              user.getUsername(),
+              user.getName(),
+              user.getSurname(),
+              user.getImgUrl(),
+              user.getEmail(),
+              UserLeaderResourceFromEntityAssembler.toResourceFromEntity(user.getLeader()),
+              null,
+              roles);
+        } else if (user.getMember() != null) {
+        return new UserResource(
+                user.getId(),
+                user.getUsername(),
+                user.getName(),
+                user.getSurname(),
+                user.getImgUrl(),
+                user.getEmail(),
+                null,
+                UserMemberResourceFromEntityAssembler.toResourceFromEntity(user.getMember()),
+                roles);
+        }else{
+          return new UserResource(
+                  user.getId(),
+                  user.getUsername(),
+                  user.getName(),
+                  user.getSurname(),
+                  user.getImgUrl(),
+                  user.getEmail(),
+                  null,
+                  null,
+                  roles);
+        }
   }
 }
