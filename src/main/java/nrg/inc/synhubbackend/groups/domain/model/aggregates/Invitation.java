2e3e4044c9b606abd3a58dcfd61b70e873fa1446
neor1se <neorise456@gmail.com>
2025-05-31 18:36:32 -0500

    feat(invitations): added invitation entity.

---
+++ src/main/java/nrg/inc/synhubbackend/groups/domain/model/aggregates/Invitation.java
@@ -0,0 +1,22 @@
+package nrg.inc.synhubbackend.groups.domain.model.aggregates;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import nrg.inc.synhubbackend.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import nrg.inc.synhubbackend.tasks.domain.model.aggregates.Member;
+
+@Entity
+@Setter
+@Getter
+public class Invitation extends AuditableAbstractAggregateRoot<Invitation> {
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    public Invitation() {}
+}
