baa0a76494eb5f0795efe4a3f234a99dff449422
cwassointt <u202112078@upc.edu.pe>
2025-06-01 23:52:22 -0500

    feat(groups): define GroupQueryService interface

--- src/main/java/nrg/inc/synhubbackend/groups/domain/services/GroupQueryService.java
+++ src/main/java/nrg/inc/synhubbackend/groups/domain/services/GroupQueryService.java
@@ -2,6 +2,7 @@ package nrg.inc.synhubbackend.groups.domain.services;
 
 import nrg.inc.synhubbackend.groups.domain.model.aggregates.Group;
 import nrg.inc.synhubbackend.groups.domain.model.queries.GetGroupByCodeQuery;
+import nrg.inc.synhubbackend.groups.domain.model.queries.GetGroupByIdQuery;
 import nrg.inc.synhubbackend.groups.domain.model.queries.GetGroupByLeaderIdQuery;
 import nrg.inc.synhubbackend.groups.domain.model.queries.GetGroupByMemberIdQuery;
 import nrg.inc.synhubbackend.iam.domain.model.aggregates.User;
@@ -13,4 +14,5 @@ public interface GroupQueryService {
     Optional<Group> handle(GetGroupByLeaderIdQuery query);
     Optional<Group> handle(GetGroupByCodeQuery query);
     Optional<Group> handle(GetGroupByMemberIdQuery query);
+    Optional<Group> handle(GetGroupByIdQuery query);
 }
