5a70ad4542dc5af8d51837962f7a1c41c6fbed6e
cwassointt <u202112078@upc.edu.pe>
2025-06-01 23:51:41 -0500

    feat(metrics): introduce GroupMetrics domain model

---
+++ src/main/java/nrg/inc/synhubbackend/metrics/domain/model/GroupMetrics.java
@@ -0,0 +1,19 @@
+package nrg.inc.synhubbackend.metrics.domain.model;
+
+public class GroupMetrics {
+    private Long groupId;
+    private int memberCount;
+
+    public GroupMetrics(Long groupId, int memberCount) {
+        this.groupId = groupId;
+        this.memberCount = memberCount;
+    }
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public int getMemberCount() {
+        return memberCount;
+    }
+}
