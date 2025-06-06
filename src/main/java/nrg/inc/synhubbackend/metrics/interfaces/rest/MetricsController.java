a189318207717fd1667d513a16bb9ebcd56aff0d
cwassointt <u202112078@upc.edu.pe>
2025-06-05 20:09:27 -0500

    feat(metrics): implement MetricsController with task and group metrics endpoints

--- src/main/java/nrg/inc/synhubbackend/metrics/interfaces/rest/MetricsController.java
+++ src/main/java/nrg/inc/synhubbackend/metrics/interfaces/rest/MetricsController.java
@@ -1,3 +1,4 @@
+// src/main/java/nrg/inc/synhubbackend/metrics/interfaces/rest/MetricsController.java
 package nrg.inc.synhubbackend.metrics.interfaces.rest;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -5,7 +6,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import nrg.inc.synhubbackend.groups.domain.model.queries.GetGroupByIdQuery;
 import nrg.inc.synhubbackend.groups.domain.services.GroupQueryService;
 import nrg.inc.synhubbackend.metrics.application.service.GroupMetricsService;
+import nrg.inc.synhubbackend.metrics.application.service.TaskMetricsService;
 import nrg.inc.synhubbackend.metrics.domain.model.GroupMetrics;
+import nrg.inc.synhubbackend.metrics.interfaces.resources.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,10 +19,12 @@ public class MetricsController {
 
     private final GroupMetricsService groupMetricsService;
     private final GroupQueryService groupQueryService;
+    private final TaskMetricsService taskMetricsService;
 
-    public MetricsController(GroupMetricsService groupMetricsService, GroupQueryService groupQueryService) {
+    public MetricsController(GroupMetricsService groupMetricsService, GroupQueryService groupQueryService, TaskMetricsService taskMetricsService) {
         this.groupMetricsService = groupMetricsService;
         this.groupQueryService = groupQueryService;
+        this.taskMetricsService = taskMetricsService;
     }
 
     @GetMapping("/groups/{groupId}/member-count")
@@ -35,5 +40,45 @@ public class MetricsController {
         return groupQueryService.handle(new GetGroupByIdQuery(groupId))
                 .map(group -> ResponseEntity.ok(group.getMemberCount()))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @Operation(
+            summary = "Get time passed for a completed task",
+            description = "Returns the time passed in milliseconds for a completed task.",
+            tags = {"Metrics"}
+    )
+    @GetMapping("/task/{taskId}/time-passed")
+    public TaskTimePassedResource getTaskTimePassed(@PathVariable Long taskId) {
+        return taskMetricsService.getTaskTimePassed(taskId);
+    }
+    
+    @GetMapping("/tasks/overview/{groupId}")
+    @Operation(summary = "Get task overview for group", description = "Returns general task status count for a group")
+    public TaskOverviewResource getTaskOverview(@PathVariable Long groupId) {
+        return taskMetricsService.getTaskOverview(groupId);
+    }
+
+    @GetMapping("/tasks/distribution/{groupId}")
+    @Operation(summary = "Get task distribution for group", description = "Returns the number of tasks per member in a group")
+    public TaskDistributionResource getTaskDistribution(@PathVariable Long groupId) {
+        return taskMetricsService.getTaskDistribution(groupId);
+    }
+
+    @GetMapping("/tasks/rescheduled/{groupId}")
+    @Operation(summary = "Get rescheduled tasks", description = "Returns the count of rescheduled vs non-rescheduled tasks")
+    public RescheduledTasksResource getRescheduledTasks(@PathVariable Long groupId) {
+        return taskMetricsService.getRescheduledTasks(groupId);
+    }
+
+    @GetMapping("/tasks/avg-solution-time/{leaderId}")
+    @Operation(summary = "Get average solution time", description = "Returns the average solution time in days for tasks completed by a leader")
+    public AvgSolutionTimeResource getAvgSolutionTime(@PathVariable Long leaderId) {
+        return taskMetricsService.getAvgSolutionTime(leaderId);
+    }
+
+    @GetMapping("/tasks/avg-dev-time/{memberId}")
+    @Operation(summary = "Get average development time", description = "Returns the average time spent on tasks by a member")
+    public AvgDevelopmentTimeResource getAvgDevTime(@PathVariable Long memberId) {
+        return taskMetricsService.getAvgDevTime(memberId);
     }
 }
