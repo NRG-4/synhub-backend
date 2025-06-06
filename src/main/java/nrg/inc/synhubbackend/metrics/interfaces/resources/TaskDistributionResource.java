c2bc286796d376ebfe15da528fc279e5cd694246
cwassointt <u202112078@upc.edu.pe>
2025-06-05 20:09:46 -0500

    feat(metrics): add TaskDistributionResource for task distribution per member

---
+++ src/main/java/nrg/inc/synhubbackend/metrics/interfaces/resources/TaskDistributionResource.java
@@ -0,0 +1,16 @@
+package nrg.inc.synhubbackend.metrics.interfaces.resources;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public class TaskDistributionResource {
+    private String type;
+    private int value;
+    private Map<String, Integer> details;
+    private String context;
+    private String summary;
+}
