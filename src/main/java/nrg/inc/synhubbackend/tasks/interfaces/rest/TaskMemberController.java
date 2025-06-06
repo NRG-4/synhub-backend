b8414ee8b7418cce2c86e6fb0b01e88d838fb909
Firtness <U202122484@upc.edu.pe>
2025-06-06 13:01:56 -0500

    feat(auth): added tasks fetching by authenticated member.

--- src/main/java/nrg/inc/synhubbackend/tasks/interfaces/rest/TaskMemberController.java
+++ src/main/java/nrg/inc/synhubbackend/tasks/interfaces/rest/TaskMemberController.java
@@ -5,6 +5,8 @@ import io.jsonwebtoken.Jwts;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import nrg.inc.synhubbackend.tasks.domain.model.queries.GetAllTasksByMemberId;
+import nrg.inc.synhubbackend.tasks.domain.model.queries.GetMemberByUsernameQuery;
+import nrg.inc.synhubbackend.tasks.domain.services.MemberQueryService;
 import nrg.inc.synhubbackend.tasks.domain.services.TaskCommandService;
 import nrg.inc.synhubbackend.tasks.domain.services.TaskQueryService;
 import nrg.inc.synhubbackend.tasks.interfaces.rest.resources.CreateTaskResource;
@@ -20,19 +22,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1/members/{memberId}/tasks")
+@RequestMapping("/api/v1/members")
 @Tag(name = "Tasks Member ", description = "Tasks Member endpoints")
 @CrossOrigin(origins = "*", methods = { RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE })
 public class TaskMemberController {
     private final TaskCommandService taskCommandService;
     private final TaskQueryService taskQueryService;
-
-    public TaskMemberController(TaskCommandService taskCommandService, TaskQueryService taskQueryService) {
+    private final MemberQueryService memberQueryService;
+    public TaskMemberController(TaskCommandService taskCommandService, TaskQueryService taskQueryService, MemberQueryService memberQueryService) {
         this.taskCommandService = taskCommandService;
         this.taskQueryService = taskQueryService;
+        this.memberQueryService = memberQueryService;
     }
 
-    @PostMapping
+    @PostMapping("/{memberId}/tasks")
     @Operation(summary = "Create a new task", description = "Creates a new task")
     public ResponseEntity<TaskResource> createTask(@PathVariable Long memberId, @RequestBody CreateTaskResource resource) {
         var createTaskCommand = CreateTaskCommandFromResourceAssembler.toCommandFromResource(resource, memberId);
@@ -45,7 +48,7 @@ public class TaskMemberController {
         return new ResponseEntity<>(taskResource, HttpStatus.valueOf(201));
     }
 
-    @GetMapping
+    @GetMapping("/{memberId}/tasks")
     @Operation(summary = "Get all tasks by member id", description = "Get all tasks by member id")
     public ResponseEntity<List<TaskResource>> getAllTasksByMemberId(@PathVariable Long memberId) {
         var getAllTasksByMemberId = new GetAllTasksByMemberId(memberId);
@@ -53,6 +56,28 @@ public class TaskMemberController {
         var taskResources = tasks.stream()
                 .map(TaskResourceFromEntityAssembler::toResourceFromEntity)
                 .collect(Collectors.toList());
+        return ResponseEntity.ok(taskResources);
+    }
+
+    @GetMapping("/tasks")
+    @Operation(summary = "Get all tasks by authenticated member", description = "Fetches all tasks for the authenticated member.")
+    public ResponseEntity<List<TaskResource>> getAllTasksByMemberAuthenticated(Authentication authentication) {
+        String username = authentication.getName();
+
+        var getMemberByUsernameQuery = new GetMemberByUsernameQuery(username);
+
+        var member = this.memberQueryService.handle(getMemberByUsernameQuery);
+
+        if(member.isEmpty()) return ResponseEntity.notFound().build();
+
+        var getAllTasksByMemberId = new GetAllTasksByMemberId(member.get().getId());
+
+        var tasks = taskQueryService.handle(getAllTasksByMemberId);
+
+        var taskResources = tasks.stream()
+                .map(TaskResourceFromEntityAssembler::toResourceFromEntity)
+                .collect(Collectors.toList());
+
         return ResponseEntity.ok(taskResources);
     }
 }
