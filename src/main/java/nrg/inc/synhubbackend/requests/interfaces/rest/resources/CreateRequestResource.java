1b1b473f76781b28710d28e8798d20bfa9e3f9c6
pachieeee <sebaspacheco2003@gmail.com>
2025-06-12 12:50:31 -0500

    fix(resources): change create request resource to adapt to path variable.

--- src/main/java/nrg/inc/synhubbackend/requests/interfaces/rest/resources/CreateRequestResource.java
+++ src/main/java/nrg/inc/synhubbackend/requests/interfaces/rest/resources/CreateRequestResource.java
@@ -3,7 +3,6 @@ package nrg.inc.synhubbackend.requests.interfaces.rest.resources;
 public record CreateRequestResource(
         String description,
         String requestType,
-        Long taskId,
         Long  memberId
 ) {
 }
--- src/main/java/nrg/inc/synhubbackend/requests/interfaces/rest/transform/CreateRequestCommandFromResourceAssembler.java
+++ src/main/java/nrg/inc/synhubbackend/requests/interfaces/rest/transform/CreateRequestCommandFromResourceAssembler.java
@@ -4,11 +4,11 @@ import nrg.inc.synhubbackend.requests.domain.model.commands.CreateRequestCommand
 import nrg.inc.synhubbackend.requests.interfaces.rest.resources.CreateRequestResource;
 
 public class CreateRequestCommandFromResourceAssembler {
-    public static CreateRequestCommand toCommandFromResource(CreateRequestResource resource) {
+    public static CreateRequestCommand toCommandFromResource(CreateRequestResource resource, Long taskId) {
         return new CreateRequestCommand(
                 resource.description(),
                 resource.requestType(),
-                resource.taskId(),
+                taskId,
                 resource.memberId()
         );
     }
