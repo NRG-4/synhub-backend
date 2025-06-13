b2b43d5d0f324bbfc7d1ab02f75af7cb35263007
cwassointt <u202112078@upc.edu.pe>
2025-06-12 20:49:25 -0500

    chore(metrics): remove unused resources

--- src/main/java/nrg/inc/synhubbackend/metrics/interfaces/rest/resources/AvgSolutionTimeResource.java
+++
@@ -1,9 +0,0 @@
-package nrg.inc.synhubbackend.metrics.interfaces.rest.resources;
-
-import java.util.Map;
-
-public record AvgSolutionTimeResource(
-        String type,
-        double value,
-        Map<String, Integer> details
-) {}
