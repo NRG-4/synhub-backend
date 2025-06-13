6c57eac32ae689f8ab817af65ed6ca57a9e9f8f0
cwassointt <u202112078@upc.edu.pe>
2025-06-12 20:22:07 -0500

    chore(metrics): remove unused AvgDevTimeResource and TaskMemberInfo records

--- src/main/java/nrg/inc/synhubbackend/metrics/interfaces/resources/AvgDevTimeResource.java
+++
@@ -1,9 +0,0 @@
-package nrg.inc.synhubbackend.metrics.interfaces.resources;
-
-import java.util.Map;
-
-public record AvgDevTimeResource(
-        String type,
-        double value,
-        Map<String, Integer> details
-) {}
--- src/main/java/nrg/inc/synhubbackend/metrics/interfaces/resources/TaskMemberInfo.java
+++
@@ -1,3 +0,0 @@
-package nrg.inc.synhubbackend.metrics.interfaces.resources;
-
-public record TaskMemberInfo(Long taskId, String memberName) {}
