fb4cc4c5e0c7ce3387f62583258ea3fb753a24b6
cwassointt <u202112078@upc.edu.pe>
2025-06-12 23:44:04 -0500

    chore(metrics): remove obsolete GroupMetricsService and GroupMetricsServiceImpl

--- src/main/java/nrg/inc/synhubbackend/metrics/domain/model/services/GroupMetricsService.java
+++
@@ -1,8 +0,0 @@
-package nrg.inc.synhubbackend.metrics.domain.model.services;
-
-import nrg.inc.synhubbackend.metrics.domain.model.aggregates.GroupMetrics;
-import java.util.Optional;
-
-public interface GroupMetricsService {
-    Optional<GroupMetrics> getGroupMetrics(Long groupId);
-}
