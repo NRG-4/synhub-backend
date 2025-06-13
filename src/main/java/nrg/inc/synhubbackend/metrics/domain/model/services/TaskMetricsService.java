b928262a6bca4dd438800fa92da58ebd5c8e4126
cwassointt <u202112078@upc.edu.pe>
2025-06-12 20:22:47 -0500

    feat(metrics): update TaskMetricsService interface for task analytics

--- src/main/java/nrg/inc/synhubbackend/metrics/domain/model/services/TaskMetricsService.java
+++ src/main/java/nrg/inc/synhubbackend/metrics/domain/model/services/TaskMetricsService.java
@@ -1,7 +1,6 @@
 package nrg.inc.synhubbackend.metrics.domain.model.services;
 
-import nrg.inc.synhubbackend.metrics.interfaces.resources.*;
-import java.util.Optional;
+import nrg.inc.synhubbackend.metrics.interfaces.rest.resources.*;
 
 public interface TaskMetricsService {
     TaskTimePassedResource getTaskTimePassed(Long taskId);
