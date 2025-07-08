b341504c9b4e46db655e3d55aec3ba53b2be7e0d
pachieeee <sebaspacheco2003@gmail.com>
2025-07-08 00:35:46 -0500

    fix(requests): update RequestRepository to return a list of requests by taskId and add deleteByTaskId method.

--- src/main/java/nrg/inc/synhubbackend/requests/infrastructure/persistence/jpa/repositories/RequestRepository.java
+++ src/main/java/nrg/inc/synhubbackend/requests/infrastructure/persistence/jpa/repositories/RequestRepository.java
@@ -4,10 +4,11 @@ import nrg.inc.synhubbackend.requests.domain.model.aggregates.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
-    boolean existsByTaskId(Long taskId);
-    Optional<Request> findByTaskId(Long taskId);
+    List<Request> findByTaskId(Long taskId);
+    void deleteByTaskId(Long taskId);
 }
